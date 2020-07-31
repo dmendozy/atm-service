bbcac57c152b56df1dd372007aca55d0028345ee
dmendozy <dmendozy@everis.com>
2020-07-31 14:26:03 -0500

    updates

--- src/main/java/com/atm/atm/AtmApplication.java
+++ src/main/java/com/atm/atm/AtmApplication.java
@@ -2,8 +2,10 @@ package com.atm.atm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AtmApplication {
 
 	public static void main(String[] args) {
