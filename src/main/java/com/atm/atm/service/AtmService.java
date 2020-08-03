9cfc702b85e508d5a07f992b40e3b07c542f9062
dmendozy <dmendozy@everis.com>
2020-08-03 09:38:28 -0500

    add deposit atm
    Signed-off-by: dmendozy <dmendozy@everis.com>

--- src/main/java/com/atm/atm/service/AtmService.java
+++ src/main/java/com/atm/atm/service/AtmService.java
@@ -2,6 +2,7 @@ package com.atm.atm.service;
 
 import com.atm.atm.adds.Account;
 import com.atm.atm.adds.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -9,20 +10,24 @@ import reactor.core.publisher.Mono;
 @Service
 public class AtmService {
 
+    @Autowired
+    WebClient.Builder webClientBuilder;
+
     public Mono<Account> deposit(String accountId, Double amount){
-        Transaction transaction = new Transaction("Deposit",amount);
-        Mono<Transaction> transactionMono = WebClient
-                .create("http://transaction-service/transactions")
+        Transaction transaction = new Transaction("Deposit",amount,accountId);
+        Mono<Transaction> transactionMono = webClientBuilder
+                .build()
                 .post()
+                .uri("http://localhost:8084/transactions")
                 .body(Mono.just(transaction),Transaction.class)
                 .retrieve()
                 .bodyToMono(Transaction.class);
-        return transactionMono.flatMap(t->{
-            return WebClient.create("http://account-service/accounts/atm/deposit/"+accountId+"/"+
-                    transaction.getTransactionId()+"/"+amount)
-                    .put()
-                    .retrieve()
-                    .bodyToMono(Account.class);
-        });
+        return transactionMono.flatMap(t->
+                webClientBuilder
+                .build()
+                .put()
+                .uri("http://localhost:8083/accounts/atm/deposit/"+accountId+"/"+amount)
+                .retrieve()
+                .bodyToMono(Account.class));
     }
 }
